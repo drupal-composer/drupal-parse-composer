99777f4a948432f3d3c00f9beb9c49a99e605ddd
Will Milton <wa.milton@gmail.com>
2014-07-06 11:48:46 -0500

    allow use of composer package name

--- src/ReleaseInfoFactory.php
+++ src/ReleaseInfoFactory.php
@@ -14,9 +14,17 @@ class ReleaseInfoFactory{
         '9'   => [],
     ];
 
+    private function drupalizeName($name) {
+        if (strpos($name, '/')) {
+            list(, $name) = explode('/', $name);
+        }
+        return $name;
+    }
+
     public function getReleasesForCore($name, $core)
     {
         $core = "$core";
+        $name = $this->drupalizeName($name);
         if (isset($this->releases[$core])) {
             if (!isset($this->releases[$core][$name])) {
                 $release = new ReleaseInfo(
@@ -36,6 +44,7 @@ class ReleaseInfoFactory{
     {
         $info   = [];
         $cores  = empty($cores) ? array_keys($this->releases) : $cores;
+        $name = $this->drupalizeName($name);
         foreach ($cores as $core) {
             if ($releaseInfo = $this->getReleasesForCore($name, $core)) {
                 $info[$core] = $releaseInfo;
