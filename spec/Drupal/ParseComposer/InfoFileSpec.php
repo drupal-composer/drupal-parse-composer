609342a99b985b97da730f86fae6614e72b12f23
Florian Weber <florian.weber@ueberbit.de>
2015-01-21 23:34:41 +0100

    Fix version constraints for short versions

--- spec/Drupal/ParseComposer/InfoFileSpec.php
+++ spec/Drupal/ParseComposer/InfoFileSpec.php
@@ -37,4 +37,21 @@ EOF;
         $this->beConstructedWith('foo', $fooInfo, 7);
         $this->constraint('bar (7.x-2.x-dev)')->shouldReturn(['drupal/bar' => '7.2.x-dev']);
     }
+
+    function it_understands_operators_in_constraints()
+    {
+        $fooInfo = '';
+        $this->beConstructedWith('foo', $fooInfo, 7);
+
+        // Valid constraints in Drupal: https://www.drupal.org/node/542202#dependencies
+        // Valid constraints in composer: https://getcomposer.org/doc/01-basic-usage.md#package-versions
+
+        $this->constraint('bar (1.0)')->shouldReturn(['drupal/bar' => '7.1.0']);
+        $this->constraint('bar (1.x)')->shouldReturn(['drupal/bar' => '7.1.*']);
+        $this->constraint('bar (>1.0, <=3.2, !=3.0)')->shouldReturn(['drupal/bar' => '>7.1.0, <=7.3.2, !=7.3.0']);
+        $this->constraint('bar (>1.0)')->shouldReturn(['drupal/bar' => '>7.1.0']);
+        $this->constraint('bar (>7.x-1.5)')->shouldReturn(['drupal/bar' => '>7.1.5']);
+        $this->constraint('system (>=7.53)')->shouldReturn(['drupal/system' => '>=7.53.0']);
+        $this->constraint('not_core (>=7.53)')->shouldReturn(['drupal/not_core' => '>=7.7.53']);
+    }
 }
