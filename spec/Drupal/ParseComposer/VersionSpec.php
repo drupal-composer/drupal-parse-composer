7721447239cc3a0a250134ed50ba06d8391aafd2
Florian Weber <florian.weber@ueberbit.de>
2015-02-06 18:49:56 +0100

    Extend test coverage for Version::valid

--- spec/Drupal/ParseComposer/VersionSpec.php
+++ spec/Drupal/ParseComposer/VersionSpec.php
@@ -29,4 +29,21 @@ class VersionSpec extends ObjectBehavior
         $this->getSemver()->shouldReturn('6.2.16-rc1');
     }
 
+    function it_validates_full_versions() {
+        $this->beConstructedWith('7.x-2.4-beta');
+
+        $this::valid('7.x-2.0')->shouldReturn(TRUE);
+        $this::valid('7.x-2.x-dev')->shouldReturn(TRUE);
+        $this::valid('7.x-2.4-beta3')->shouldReturn(TRUE);
+        $this::valid('6.x-2.16-rc1')->shouldReturn(TRUE);
+
+        /**
+         * Some drupal.org repositories contain invalid tags.
+         * - http://cgit.drupalcode.org/backup_migrate/refs/tags
+         * - http://cgit.drupalcode.org/migrate/refs/tags
+         */
+        $this::valid('7.x-2.x-beta1')->shouldReturn(FALSE);
+        $this::valid('7.x-3.x-alpha')->shouldReturn(FALSE);
+    }
+
 }
