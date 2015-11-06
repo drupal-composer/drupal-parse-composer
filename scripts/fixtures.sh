28c33762a5906dadcd6dbd4dde569066795df394
Florian Weber <florian.weber@ueberbit.de>
2015-11-06 02:15:00 +0100

    Add coder module as fixture

--- scripts/fixtures.sh
+++ scripts/fixtures.sh
@@ -4,3 +4,4 @@ git clone http://git.drupal.org/project/omega res/omega
 git clone http://git.drupal.org/project/master res/master
 git clone http://git.drupal.org/project/registry_rebuild res/registry_rebuild
 git clone http://git.drupal.org/project/config_devel res/config_devel
+git clone http://git.drupal.org/project/coder res/coder
